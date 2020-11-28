74554db4ea1e0c52188a579360675d2190169afe
owenmoogk <owenmoogk@gmail.com>
2020-11-27 22:24:33 -0500

    finished it up

--- xml-projects-page/load-project-page.js
+++ xml-projects-page/load-project-page.js
@@ -1,11 +1,11 @@
-function loadProjectPage() {
+function loadProjectPage(xmlPage) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			projectPage(this);
 		}
     };
-    xmlhttp.open("GET", "/pagetest.xml", true);
+    xmlhttp.open("GET", xmlPage, true);
     xmlhttp.send();
 }
 function projectPage(xml) {
@@ -22,7 +22,6 @@ function projectPage(xml) {
 
 
     // load main part of the page
-    var currBlock, txt; 
     blocks = xmlDoc.getElementsByTagName("block");
     for (currBlockNum = 0; currBlockNum < blocks.length; currBlockNum++) {
         var currBlockLength = blocks[currBlockNum].children.length
@@ -30,14 +29,25 @@ function projectPage(xml) {
         var title = currBlock.getElementsByTagName('title')[0].innerHTML
         var text = currBlock.getElementsByTagName('text')[0].innerHTML
         var image;
-        if (currBlockNum == 3){
+        txt += '<div class="block"><div class="text"><h1>'+title+'</h1><p>'+text+'</p></div>';
+        if (currBlockLength == 3){
             image = currBlock.getElementsByTagName('image')[0].innerHTML
+            txt += '<div class="img"><img src="'+image+'" class="img"></div>'
         }
-        txt += '<div class="block"><div class="text"><h1>Overview</h1><p>text</p></div><div class="img"><img src="/projects/2702-2019/img/robot.png" alt="Robot" class="img"></div></div>';
+        txt += '</div>'
     }
 
     // load links at the bottom
-    links = xmlDoc.getElementsByTagName
+    links = xmlDoc.getElementsByTagName('link')
+    txt += '<div class="block"><div class="text"><h1>Resources</h1><p><ul>'
+    for (currLinkNum = 0; currLinkNum < links.length; currLinkNum++) {
+        var currLink = links[currLinkNum]
+        console.log(currLink)
+        var linkText = currLink.getElementsByTagName('link-text')[0].innerHTML
+        var src = currLink.getElementsByTagName('src')[0].innerHTML
+        txt += '<li><a href="'+src+'" class="blinks" target="_blank">'+linkText+'</a></li>';
+    }
+    txt += '</ul></p></div></div>'
 
     document.getElementsByClassName("body")[0].innerHTML = txt;
 }
