4764538ad28102a4033d41930c5408ba554f7a87
owenmoogk <owenmoogk@gmail.com>
2020-11-27 20:04:07 -0500

    working towards project page
    still have to do images

--- xml-projects-page/load-project-page.js
+++ xml-projects-page/load-project-page.js
@@ -9,31 +9,35 @@ function loadProjectPage() {
     xmlhttp.send();
 }
 function projectPage(xml) {
-    var currBlock, txt, xmlDoc; 
-    xmlDoc = xml.responseXML;
-    txt = "";
+
+    var xmlDoc;
+    xmlDoc = xml.responseXML; // xml doxument
+
+    txt = ""; // entire html of page
+
+    // load header of page
+    var pageTitle = xmlDoc.getElementsByTagName('page-title')[0].innerHTML
+    var date = xmlDoc.getElementsByTagName('date')[0].innerHTML
+    txt += '<div class="title"><p style="line-height: 70px;">'+pageTitle+'</p></div><div class="date"><p class="pdate">'+date+'</p></div>';
+
+
+    // load main part of the page
+    var currBlock, txt; 
     blocks = xmlDoc.getElementsByTagName("block");
     for (currBlockNum = 0; currBlockNum < blocks.length; currBlockNum++) {
         var currBlockLength = blocks[currBlockNum].children.length
         var currBlock = blocks[currBlockNum]
         var title = currBlock.getElementsByTagName('title')[0].innerHTML
         var text = currBlock.getElementsByTagName('text')[0].innerHTML
-        var image
+        var image;
         if (currBlockNum == 3){
-            image = currBlock.getElementsByTagName('text')[0].innerHTML
+            image = currBlock.getElementsByTagName('image')[0].innerHTML
         }
-        var linkLocations = []
-        var continueLoop = true
-        while (continueLoop){
-            linkLocations.push(text.search('~~'))
-            text = text.replace('~~','')
-            console.log(linkLocations)
-            if (!text.includes('~~')){
-                continueLoop = false
-            }
-        }
-
-        txt += '<a href = '+links[i].childNodes[0].nodeValue+' class = "navlinks">'+titles[i].childNodes[0].nodeValue+'</a>';
+        txt += '<div class="block"><div class="text"><h1>Overview</h1><p>text</p></div><div class="img"><img src="/projects/2702-2019/img/robot.png" alt="Robot" class="img"></div></div>';
     }
-    document.getElementById("main").innerHTML = txt;
+
+    // load links at the bottom
+    links = xmlDoc.getElementsByTagName
+
+    document.getElementsByClassName("body")[0].innerHTML = txt;
 }
