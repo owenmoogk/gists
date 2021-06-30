80f6305e1a9b0670bb10f3ad8e9c35e80cd5d1cb
Owen Moogk <owenmoogk@gmail.com>
2021-06-30 19:10:26 -0400

    fixed the overlap between the keybind and toggle, and returns to default caps :key:

--- sticky-caps.py
+++ sticky-caps.py
@@ -6,21 +6,29 @@ hotkey = "alt + i"
 active = False # if the sticky caps is active
 held = False # if the hotkey is currently held
 case = True # just alternates for caps and not
+isCapsOn = False # even if this isnt actually accurate, it returns the caps state to the state it was before hitting the keybind
 
 def activate():
 
 	def onPress(key):
-		global active, held, case
-		
-		if (len(str(key)) == 3) and active:
-			case = not case
-			pyautogui.press('capslock')
+		global active, held, case, isCapsOn
 
 		if keyboard.is_pressed(hotkey) and (not held):
 			active = not active
 			held = True
+
+			# return the caps state to the og state
+			if isCapsOn:
+				pyautogui.press('capslock')
+				isCapsOn = False
 		else:
 			held = False
+
+			if (len(str(key)) == 3) and active:
+				case = not case
+				pyautogui.press('capslock')
+				isCapsOn = not isCapsOn
+		
 		
 	def onRelease(key):
 		pass
