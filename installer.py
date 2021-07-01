d56b9fbda2f18249e054492920cf44ac8c8f5151
Owen Moogk <owenmoogk@gmail.com>
2021-06-30 20:43:55 -0400

    added the installer :package:

---
+++ installer.py
@@ -0,0 +1,23 @@
+import shutil
+import tkinter as tk
+
+original = 'sticky-caps.py'
+target = 'C:\\Users\\owenm\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\sticky-caps.pyw'
+
+shutil.copyfile(original, target)
+
+
+def write_slogan():
+   print("Tkinter is easy to use!")
+
+root = tk.Tk()
+root.geometry('500x200')
+root.title('Sticky Caps Installer')
+frame = tk.Frame(root)
+frame.pack()
+
+text = tk.Text(frame, height=10, width=50)
+text.pack()
+text.insert(tk.END, 'Sticky caps is installed! Toggle with \"ALT + I\".')
+
+root.mainloop()
