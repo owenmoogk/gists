2bdd1107b9bb6b2cb5f250ceaee5da1f18c8d6db
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2022-06-03 23:10:29 -0400

    looking at different probability distributions from data management class

---
+++ probability/main.py
@@ -0,0 +1,27 @@
+import random
+import matplotlib.pyplot as plt
+
+austinLikesCoins = {
+  0:0,
+  1:0,
+  2:0,
+  3:0,
+  4:0
+}
+
+trials = int(input("Enter the amount of trials: "))
+
+for i in range(trials):
+  counter = 0
+  for i in range(4):
+    dice = random.randint(1,6)
+
+    if dice == 1:
+      counter += 1
+
+  austinLikesCoins[counter] += 1
+
+print (austinLikesCoins)
+
+plt.plot([0,1,2,3,4], list(austinLikesCoins.values()))
+plt.show()
