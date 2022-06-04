a9705b4f2809b66ded8ec38885ab071d342ab61d
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2022-06-03 23:10:29 -0400

    looking at different probability distributions from data management class

---
+++ probability/dice.py
@@ -0,0 +1,29 @@
+import random
+import matplotlib.pyplot as plt
+
+austinLikesCoins = {}
+
+for i in range(2,13):
+  austinLikesCoins[i] = 0
+
+trials = int(input("Enter the amount of trials: "))
+
+for i in range(trials):
+
+  dice1 = random.randint(1,6)
+  dice2 = random.randint(1,6)
+
+  sum = dice1 + dice2
+
+  austinLikesCoins[sum] += 1
+
+print (austinLikesCoins)
+
+sum = 0
+for i in range(2,13):
+  sum += austinLikesCoins[i]
+for i in range(2,13):
+  austinLikesCoins[i] = austinLikesCoins[i] / sum
+
+plt.plot(range(2,13), list(austinLikesCoins.values()))
+plt.show()
